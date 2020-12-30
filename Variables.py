8b8c1ca2c39af7eac8b9443407752e72898adaeb
Riveni <riven.eng@gmail.com>
2020-12-30 17:14:07 +0200

    Variables info

---
+++ Variables.py
@@ -0,0 +1,12 @@
+# Variables are pointers that points on object
+# Variables in python are dynamic and there is no need to declare them
+# since their type is defined while the program run
+# each object has a defined type ( int, string, float... )
+
+x = 'object'  # string type
+y = "learner"  # string type
+z = 5  # int type
+w = 5.0  # float type
+n = None  # NULL
+
+type(x)  #  function that returns variable type
