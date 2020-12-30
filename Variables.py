76dcea1e380152fb570a6e61bc754cf8c39866c6
Riveni <riven.eng@gmail.com>
2020-12-30 17:21:24 +0200

    Input/Output info

--- Variables.py
+++ Variables.py
@@ -10,3 +10,17 @@ w = 5.0  # float type
 n = None  # NULL
 
 type(x)  #  function that returns variable type
+
+# Output data to screen
+print("Hello " + y + "!")
+
+# Input data to variable
+x = input("text you can print:")  # input defined as string!
+
+# Casting: converting variable from one type to another
+y = int(x)  # cast string to int
+
+# Switching variables:
+v1 = 5
+v2 = 7
+v1, v2 = v2, v1
