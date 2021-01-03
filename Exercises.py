82bd4637de70bc48fe82e4ef3888770240a1cb4d
Riveni <riven.eng@gmail.com>
2021-01-03 21:26:40 +0200

    Ex.2

--- Exercises.py
+++ Exercises.py
@@ -11,3 +11,12 @@ divide = bricks_sum // 3
 redundancy = bricks_sum % 3
 print(bricks_sum, divide, redundancy, redundancy == 0, sep="\n")
 """
+
+# Ex.2
+"""
+print("\"Shuffle, Shuffle, Shuffle\", say it together!\n"
+      "Change colors and directions, \n"
+      "Don't back down and stop the player!\n"
+      "\tDo you want to play Taki?\n"
+      "\tPress y\\n")
+"""
