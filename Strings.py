631993b1fffe3a707c9370cc1d286dc704212b72
Riveni <riven.eng@gmail.com>
2021-01-03 21:52:09 +0200

    Strings info

--- Strings.py
+++ Strings.py
@@ -12,3 +12,14 @@ str4 = str1 * 2  # multiple strings
 """ 
   triple quotes allows typing without using special chars and also for long comments
 """
+
+# Accessing char in string
+char1 = str1[0]
+char2 = str1[-1]  # (-1) is the last char in the string
+# Strings slicing
+str1[0:4]  # return chars 0-3
+# Slicing with step: string[START:STOP:STEP]
+
+# String type is immutable, meaning it is not possible to change single char
+# in existing string: str1[0] = 'a' -> ERROR
+# Therefore, in order to change a char in string we have to change the whole string
