76a1396d47edc9546a16af8be7eafc8cd3444c0d
Riveni <riven.eng@gmail.com>
2021-01-03 21:21:39 +0200

    Strings info

---
+++ Strings.py
@@ -0,0 +1,14 @@
+# Creating a string can be done by using "" or ''
+str1 = "this is string"
+str2 = 'this is also'
+
+# Strings arithmetic
+str3 = str1 + str2  # joining strings
+str4 = str1 * 2  # multiple strings
+
+# Special chars
+# \n :new line
+# \t : tab
+""" 
+  triple quotes allows typing without using special chars and also for long comments
+"""
