393af252148b38022dd6bec431a257607d0475e3
Riveni <riven.eng@gmail.com>
2021-01-04 22:44:58 +0200

    Ex.6

--- Exercises.py
+++ Exercises.py
@@ -43,3 +43,25 @@ if word == rev:
 else:
     print("NOT")
 """
+
+# Ex.6
+"""
+import calendar
+date = input("Enter a date: ")
+day, month, year = date.split("/")
+week_day = calendar.weekday(int(year), int(month), int(day))
+if week_day == 0:
+    print("Monday")
+elif week_day == 1:
+    print("Tuesday")
+elif week_day == 2:
+    print("Wednesday")
+elif week_day == 3:
+    print("Thursday")
+elif week_day == 4:
+    print("Friday")
+elif week_day == 5:
+    print("Saturday")
+elif week_day == 6:
+    print("Sunday")
+"""
