160777acc6b7d1c607fdc1f6f6c9a3f107b95f36
Riveni <riven.eng@gmail.com>
2021-01-06 13:10:40 +0200

    Lists info

---
+++ Lists.py
@@ -0,0 +1,17 @@
+# List is a data structure that allows us to combine multiple objects to one
+empty_list = []
+empty_list2 = list()
+
+# List can hold different types of objects (int, float, strings and another lists)
+multi_type_list = [5, 5.5, "star", ["list2"]]
+list_of_letters = list("letters")  # [ 'l', 'e', 't', 't', 'e', 'r', 's' ]
+
+# Accessing lists element
+first_element = multi_type_list[0]
+element_inside_list2 = multi_type_list[3][0]
+
+# Assigning list elemets to variables
+a, b, c, d = multi_type_list
+
+
+
