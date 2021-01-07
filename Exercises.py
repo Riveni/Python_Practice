a179ac17ad67386dd7456b2cde8551267f8e9088
Riveni <riven.eng@gmail.com>
2021-01-07 11:38:35 +0200

    Ex.15

--- Exercises.py
+++ Exercises.py
@@ -189,3 +189,24 @@ def main():
 if __name__ == "__main__":
     main()
 """
+
+# Ex.15
+"""
+def are_lists_equal(list1, list2):
+    if sorted(list2) in [sorted(list1)] and sorted(list1) in [sorted(list2)]:
+        return True
+    else:
+        return False
+
+
+def main():
+    list1 = [0.6, 1, 2, 3]
+    list2 = [3, 2, 0.6, 1]
+    list3 = [9, 0, 5, 10.5]
+    print(are_lists_equal(list1, list2))
+    print(are_lists_equal(list1, list3))
+
+
+if __name__ == "__main__":
+    main()
+"""
