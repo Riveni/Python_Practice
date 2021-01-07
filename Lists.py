e888c36fffe01a66c9a7b31d8ef65dae63766ab8
Riveni <riven.eng@gmail.com>
2021-01-07 11:35:41 +0200

    Lists info

--- Lists.py
+++ Lists.py
@@ -10,8 +10,13 @@ list_of_letters = list("letters")  # [ 'l', 'e', 't', 't', 'e', 'r', 's' ]
 first_element = multi_type_list[0]
 element_inside_list2 = multi_type_list[3][0]
 
-# Assigning list elemets to variables
+# Assigning list elements to variables
 a, b, c, d = multi_type_list
+
+# Lists are mutable: it is possible to change, add or delete elements from it
+empty_list.append("1")  # add element to the end of the list
+empty_list[0] = "2"  # changing existing element
+empty_list.remove("2")  # remove element by its name
 
 
 
