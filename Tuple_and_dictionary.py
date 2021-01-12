0278279b4002e0da88dd3fc83e86bad1b4695953
Riveni <riven.eng@gmail.com>
2021-01-12 20:48:01 +0200

    Dictionary info

--- Tuple_and_dictionary.py
+++ Tuple_and_dictionary.py
@@ -12,3 +12,26 @@ single_element_tuple = ("one",)
 # String formatting
 basket_content = 3, "wine"
 print("The basket contains %d bottles of %s." % basket_content)
+
+# Dictionary is a data structure that holds values by theirs keys
+empty_dict = {}
+empty_dict2 = dict()
+my_dict = {"key": "value", "key2": "value2"}
+
+# Accessing dictionary values is done by using their key:
+dict_value = my_dict["key"]
+
+# Dictionary is mutable, and elements can be added, removed or modified
+my_dict["new key"] = "new value"
+my_dict["key"] = "modified value for existing key"
+# Deletes element from the dictionary
+del my_dict["key"]
+my_dict.pop("key2")
+
+# Dictionary view
+my_dict.keys()
+my_dict.values()
+my_dict.items()
+
+# Dictionary values can be mutable data types such as lists
+# but dictionary keys must be immutable data type!
